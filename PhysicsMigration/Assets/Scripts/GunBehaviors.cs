fe92bbb82ac74c92258638a02be2bbc53691a59d
charliekowalski <charlie.kowalski@mymail.champlain.edu>
2020-11-04 17:06:58 -0500

    added left and right rotation on gun

--- PhysicsMigration/Assets/Scripts/GunBehaviors.cs
+++ PhysicsMigration/Assets/Scripts/GunBehaviors.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GunBehaviors : MonoBehaviour
 {
+
+   public float rotateSpeed = 500.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +15,14 @@ public class GunBehaviors : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("Alpha1"))
+        if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKey(KeyCode.Alpha1))
         {
-           gameObject.transform.rotation.z
+         transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
         }
-    }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKey(KeyCode.Alpha2))
+        {
+           transform.Rotate(Vector3.back * rotateSpeed * Time.deltaTime);
+        }
+   }
 }
