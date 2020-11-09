c58af758d4309512a02e94fedd626a33e57eea46
Olli Machina <olli.machina@mymail.champlain.edu>
2020-11-08 17:05:15 -0700

    working on Target behavior

--- PhysicsMigration/Assets/Scripts/TargetBehavior.cs
+++ PhysicsMigration/Assets/Scripts/TargetBehavior.cs
@@ -13,6 +13,15 @@ public class TargetBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       // if(gameObject != pUnit)
+       //{
+       //   float distSQ = getSquaredDistance(pUnit.transform.position, gameObject.transform.position);
+       //   if(distSQ <= 1600)
+       //   {
+       //       //add to score
+       //       //spawn new one (in game manager?)
+       //       Destroy(gameObject);
+       //   }
+       //}
     }
 }
