2508e64e6f555b84825087f23153acef66c0e1cb
charliekowalski <charlie.kowalski@mymail.champlain.edu>
2020-11-08 23:34:40 -0500

    forcemanager

---
+++ PhysicsMigration/Assets/Scripts/ForceManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+static class ForceManager
+{
+   static List<ForceGenerator2D> arrayOfGenerators;
+
+   // Start is called before the first frame update
+   static void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    static void Update()
+    {
+        
+    }
+
+   static void addForceGenerator(ForceGenerator2D forceGeneratorToAdd)
+   {
+      arrayOfGenerators.Add(forceGeneratorToAdd);
+   }
+   static void removeForceGenerator(ForceGenerator2D forceGeneratorToAdd)
+   {
+      arrayOfGenerators.Remove(forceGeneratorToAdd);
+   }
+   static void updateall()
+   {
+      for (var i = 0; i < arrayOfGenerators.Count; i++)
+      {
+         arrayOfGenerators[i].;
+      }
+   }
+}
